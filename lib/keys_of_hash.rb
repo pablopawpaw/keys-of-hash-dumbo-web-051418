5fdf263e8fb09dcbd7f8d94b1f35498e27cea859
pablopawpaw <lpault29@gmail.com>
2018-05-15 10:38:53 -0400

    edited hash class with method

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,21 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of_hash(*arguments)
+    array = []
+    self.each do |key, value|
+      arguments.each do |arg|
+        if value == arg
+          array << key
+        end
+      end
+    end
+    array
   end
 end
+
+hash = {
+  "key1": "value1",
+  "key2": "value2",
+  "key3": "value3"
+}
+
+puts hash.keys_of_hash("value2","value1","value3")
